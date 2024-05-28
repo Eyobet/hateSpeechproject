052cc20d8aab1aa11286e67859bc5899f11f32fd
Eyobet <eyobzekarias23@gmail.com>
2024-05-28 09:58:06 +0300

    cleaning the data set

--- Hate.py
+++ Hate.py
@@ -11,7 +11,24 @@ from nltk.util import pr
 from nltk.corpus import stopwords
 
 data= pd.read_csv('labeled_data.csv')
-print(data.head())
+
+stemer=nltk.SnowballStemmer('english')
 
 data['lables'] =data['class'].map({0:"Hate Speech", 1:" Offesinve Language", 2:"Normal"})
 
+print(data.head())
+
+# selecting imprtant clumns
+data=data[['tweet','lables']]
+print(data.head())
+
+# cleaning the data set 
+def clean_text(text):
+    text=text.lower()
+    text=re.sub('\[.*?\]', '', text)
+    text=re.sub('https?://\S+|www\.\S+', '', text)
+    text=re.sub('<.*?>+', '', text)
+    text=re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text=re.sub('\n', '', text)
+    text=re.sub('\w*\d\w*', '', text)
+    return text
